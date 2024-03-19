56aef152e2b5b9d81ea909276a3eac2fa9df2a1b
INFy93 <infy93@gmail.com>
2024-03-19 20:18:52 +0300

    clean array before next request

--- resources/js/Composables/NetpingStates/NetpingStates.js
+++ resources/js/Composables/NetpingStates/NetpingStates.js
@@ -6,24 +6,27 @@ export default function useNetpingStates() {
     const alarm = ref([]);
     const secure = ref([]);
     const powerState = async () => {
+        power.value = [];
         let response = await axios.get('/api/power');
 
         power.value = response.data;
     }
 
     const doorState = async() => {
+        door.value = [];
         let response = await axios.get('/api/door');
         door.value = response.data
     }
 
     const alarmState = async() => {
+        alarm.value = [];
         let response = await axios.get('/api/alarm');
         alarm.value = response.data
     }
 
     const secureState = async() => {
+        secure.value = [];
         let response = await axios.get('/api/secure');
-        console.log(response.data)
         secure.value = response.data
     }
 
