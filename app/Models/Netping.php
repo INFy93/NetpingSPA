aa2a75dd08d7e10331cb16bba3118378c1524908
INFy93 <infy93@gmail.com>
2024-03-19 20:48:38 +0300

    add hasMany -> Bdcoms

--- app/Models/Netping.php
+++ app/Models/Netping.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Netping extends Model
 {
     use HasFactory;
     protected $table = 'netping';
+
+    public function bdcoms(): HasMany
+    {
+        return $this->hasMany(Bdcom::class, 'id', 'bdcom_id');
+    }
 }
