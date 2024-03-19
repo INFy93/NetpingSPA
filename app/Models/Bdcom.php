cba87c988e8486c9e700c55a8e806f83e3943f87
INFy93 <infy93@gmail.com>
2024-03-19 20:57:41 +0300

    add belongsTo -> Netping

--- app/Models/Bdcom.php
+++ app/Models/Bdcom.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Bdcom extends Model
 {
     use HasFactory;
+
+    public function netping(): BelongsTo
+    {
+        return $this->belongsTo(Netping::class, 'bdcom_id', 'id');
+    }
 }
