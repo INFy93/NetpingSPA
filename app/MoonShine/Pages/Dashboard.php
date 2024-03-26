781bffb9ab753e2d16af003f742573cdb3e4940c
INFy93 <infy93@gmail.com>
2024-03-26 11:22:52 +0300

    fill main dashboard page

--- app/MoonShine/Pages/Dashboard.php
+++ app/MoonShine/Pages/Dashboard.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Pages;
 
+use MoonShine\Components\Link;
 use MoonShine\Pages\Page;
 
 class Dashboard extends Page
@@ -17,11 +18,17 @@ class Dashboard extends Page
 
     public function title(): string
     {
-        return $this->title ?: 'Dashboard';
+        return $this->title ?: 'Управление';
     }
 
     public function components(): array
 	{
-		return [];
+		return [
+            Link::make('/admin/resource/netping-resource/index-page', 'Точки')->button(),
+            Link::make('/admin/resource/bdcom-resource/index-page', 'BDCOM')->button(),
+            Link::make('/admin/resource/user-resource/index-page', 'Пользователи')->button(),
+            Link::make('/admin/resource/action-resource/index-page', 'Действия')->button(),
+            Link::make('/admin/resource/log-resource/index-page', 'Логи')->button(),
+        ];
 	}
 }
