f4ede04822d438661795be2ac2088865bb3883c7
INFy93 <infy93@gmail.com>
2024-03-27 12:19:19 +0300

    add toast notifications

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,14 +6,33 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const options = {
+            position: "top-right",
+            transition: "Vue-Toastification__fade",
+            timeout: 3000,
+            closeOnClick: true,
+            pauseOnFocusLoss: true,
+            pauseOnHover: true,
+            draggable: true,
+            draggablePercent: 0.6,
+            showCloseButtonOnHover: false,
+            hideProgressBar: true,
+            closeButton: "button",
+            icon: true,
+            rtl: false,
+        };
+        createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(Toast, options)
             .use(ZiggyVue)
             .mount(el)
     },
