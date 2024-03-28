fce027f1ae255015e76d80605df858ba697485b5
INFy93 <infy93@gmail.com>
2024-03-28 11:46:29 +0300

    fill the migrations

--- database/migrations/2024_03_28_114410_add_telegram_user_id_to_users_table.php
+++ database/migrations/2024_03_28_114410_add_telegram_user_id_to_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('telegram_user_id')
+                ->after('email')
+                ->nullable();
         });
     }
 
