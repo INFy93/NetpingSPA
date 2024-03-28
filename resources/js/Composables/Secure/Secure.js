7680c81ada2d5f81e4aee32209075fd326d3c1e8
INFy93 <infy93@gmail.com>
2024-03-28 14:38:35 +0300

    remove toast

--- resources/js/Composables/Secure/Secure.js
+++ resources/js/Composables/Secure/Secure.js
@@ -1,6 +1,5 @@
 export default function useSecure()
 {
-    const toast = useToast();
     const switchAlarm = async (id) => {
         let response = await axios.get("/api/alarm/set/" + id);
     }
