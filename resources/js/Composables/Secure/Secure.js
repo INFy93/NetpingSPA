9929d19c9f579cc4698e6c7b792877e9e72cc2bd
INFy93 <infy93@gmail.com>
2024-03-28 14:38:23 +0300

    remove toast

--- resources/js/Composables/Secure/Secure.js
+++ resources/js/Composables/Secure/Secure.js
@@ -1,10 +1,8 @@
-import {useToast} from "vue-toastification";
 export default function useSecure()
 {
     const toast = useToast();
     const switchAlarm = async (id) => {
         let response = await axios.get("/api/alarm/set/" + id);
-        return toast.success(response.data)
     }
     return {
         switchAlarm
