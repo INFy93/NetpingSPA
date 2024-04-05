264b8bc85bd0f6b3c5f0847b784925dc495e3644
INFy93 <infy93@gmail.com>
2024-04-05 11:04:13 +0300

    prepare table for bdcoms

--- resources/js/Pages/Bdcom/Bdcom.vue
+++ resources/js/Pages/Bdcom/Bdcom.vue
@@ -1,5 +1,28 @@
 <template>
-Тут будут бдкомы
+    <table id="main_table" class="border-collapse w-full mt-3">
+        <thead>
+        <tr>
+            <th
+                class="p-3 font-bold uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400 text-gray-600 border border-gray-300 hidden lg:table-cell">
+                Имя
+            </th>
+            <th
+                class="p-3 font-bold uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400 text-gray-600 border border-gray-300 hidden lg:table-cell">
+                IP
+            </th>
+            <th
+                class="p-3 font-bold uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400 text-gray-600 border border-gray-300 hidden lg:table-cell">
+                Точка
+            </th>
+            <th
+                class="p-3 font-bold uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400 text-gray-600 border border-gray-300 hidden lg:table-cell">
+                T°
+            </th>
+        </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-500 dark:divide-gray-600">
+        </tbody>
+    </table>
 </template>
 
 <script>
