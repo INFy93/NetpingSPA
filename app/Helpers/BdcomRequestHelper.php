7b50aa0899e06211896b4710f8eaea363304fa7f
INFy93 <infy93@gmail.com>
2024-04-05 11:51:45 +0300

    modify bdcom temp helper

--- app/Helpers/BdcomRequestHelper.php
+++ app/Helpers/BdcomRequestHelper.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Client\Pool;
 use Illuminate\Support\Facades\Http;
 
-function getBdcomTemp($bdcoms): array
+function getBdcomTemp($bdcoms, $group = 1): array
 {
     $responses = Http::pool(function (Pool $pool) use ($bdcoms) {
         foreach ($bdcoms as $bdcom) {
@@ -19,38 +19,47 @@ function getBdcomTemp($bdcoms): array
     });
     $temps = [];
     $i = 0;
-    foreach ($responses as $r)
-    {
-        if (!$r instanceof ConnectionException) {
-            if (($i - 1) >= 0 && $bdcoms[$i]->netping_id == $bdcoms[$i-1]->netping_id)
-            {
-                $temps[$i]['bdcom1_temp'] = intval($temps[$i - 1]['bdcom1_temp']);
-                $temps[$i]['bdcom2_temp'] = intval(str_replace("\n","", $r->body()));
-                $temps[$i]['netping_id'] = $bdcoms[$i]->netping_id;
-                unset($temps[$i - 1]);
-            } else
-            {
-                $temps[$i]['bdcom1_temp'] = intval(str_replace("\n","", $r->body()));
-                $temps[$i]['netping_id'] = $bdcoms[$i]->netping_id;
-            }
+    foreach ($responses as $r) {
+        if ($group == 1) {
+            if (!$r instanceof ConnectionException) {
+                if (($i - 1) >= 0 && $bdcoms[$i]->netping_id == $bdcoms[$i - 1]->netping_id) {
+                    $temps[$i]['bdcom1_temp'] = intval($temps[$i - 1]['bdcom1_temp']);
+                    $temps[$i]['bdcom2_temp'] = intval(str_replace("\n", "", $r->body()));
+                    $temps[$i]['netping_id'] = $bdcoms[$i]->netping_id;
+                    $temps[$i]['bdcom_id'] = $bdcoms[$i]->id;
+                    unset($temps[$i - 1]);
+                } else {
+                    $temps[$i]['bdcom1_temp'] = intval(str_replace("\n", "", $r->body()));
+                    $temps[$i]['netping_id'] = $bdcoms[$i]->netping_id;
+                    $temps[$i]['bdcom_id'] = $bdcoms[$i]->id;
+                }
 
-            $i++;
-    } else
-        {
-            if (($i - 1) >= 0 && $bdcoms[$i]->netping_id == $bdcoms[$i-1]->netping_id)
-            {
-                $temps[$i]['bdcom1_temp'] = 0;
-                $temps[$i]['bdcom2_temp'] = 0;
-                $temps[$i]['netping_id'] = $bdcoms[$i]->netping_id;
-                unset($temps[$i - 1]);
-            } else
-            {
-                $temps[$i]['bdcom1_temp'] = 0;
-                $temps[$i]['netping_id'] = $bdcoms[$i]->netping_id;
-            }
+                $i++;
+            } else {
+                if (($i - 1) >= 0 && $bdcoms[$i]->netping_id == $bdcoms[$i - 1]->netping_id) {
+                    $temps[$i]['bdcom1_temp'] = 0;
+                    $temps[$i]['bdcom2_temp'] = 0;
+                    $temps[$i]['netping_id'] = $bdcoms[$i]->netping_id;
+                    $temps[$i]['bdcom_id'] = $bdcoms[$i]->id;
+                    unset($temps[$i - 1]);
+                } else {
+                    $temps[$i]['bdcom1_temp'] = 0;
+                    $temps[$i]['bdcom_id'] = $bdcoms[$i]->id;
+                }
 
+                $i++;
+            }
+        } else {
+            if (!$r instanceof ConnectionException) {
+                $temps[$i]['bdcom1_temp'] = intval(str_replace("\n", "", $r->body()));
+                $temps[$i]['bdcom_id'] = $bdcoms[$i]->id;
+            } else {
+                $temps[$i]['bdcom1_temp'] = 0;
+                $temps[$i]['bdcom_id'] = $bdcoms[$i]->id;
+            }
             $i++;
         }
+
     }
 
     return array_values($temps);
