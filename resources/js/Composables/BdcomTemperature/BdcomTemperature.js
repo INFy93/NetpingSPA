52607de7bba0b169be2a15dbf2679972387755be
INFy93 <infy93@gmail.com>
2024-04-05 12:04:19 +0300

    add group request - composable

--- resources/js/Composables/BdcomTemperature/BdcomTemperature.js
+++ resources/js/Composables/BdcomTemperature/BdcomTemperature.js
@@ -5,8 +5,8 @@ export default function useBdcomTemperatures()
 {
     const temps = ref([]);
 
-    const getBdcomTemps = async() => {
-        let response = await axios.get('/api/bdcoms_current_temp');
+    const getBdcomTemps = async(group) => {
+        let response = await axios.get('/api/bdcoms_current_temp?group=' + group);
 
         temps.value = response.data;
     }
