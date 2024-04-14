ec77186f91cc0ee5f21d3062b2a910f48e5b85e4
INFy93 <infy93@gmail.com>
2024-04-14 10:38:36 +0300

    add netping to database and format some fields

--- app/MoonShine/Resources/NetpingResource.php
+++ app/MoonShine/Resources/NetpingResource.php
@@ -9,7 +9,12 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Netping;
 
 use MoonShine\ActionButtons\ActionButton;
+use MoonShine\Decorations\Divider;
+use MoonShine\Fields\Checkbox;
+use MoonShine\Fields\Fields;
+use MoonShine\Fields\Hidden;
 use MoonShine\Fields\Relationships\HasMany;
+use MoonShine\MoonShineRequest;
 use MoonShine\QueryTags\QueryTag;
 use MoonShine\Resources\ModelResource;
 use MoonShine\Decorations\Block;
@@ -35,7 +40,10 @@ class NetpingResource extends ModelResource
 
     protected bool $detailInModal = true;
 
-    public function fields(): array
+    /*
+     * We don't need to display a lot of fields there. So, let's use indexFields method
+     */
+    public function indexFields(): array
     {
         return [
             Block::make([
@@ -51,6 +59,65 @@ class NetpingResource extends ModelResource
         ];
     }
 
+    /*
+     * Now, we need all fields to add them to database. But also we need to format some values (for example, alarm status link)
+     * So, we can use reactive property to format values, using IP address.
+     * This links depends on revision. We can use onChangeMethod property and public method to put on the session revision value.
+     * Usually, reactive callback (like in documentation) can change the value just only for one field.
+     * Putting values to several fields require, for example, foreach loop.
+     */
+    public function formFields(): array
+    {
+        return [
+            Divider::make('В первую очередь нужно отметить ревизию! По умолчанию ревизия v2.'), //divider
+            Checkbox::make('Ревизия (отметить, если v4)', 'revision') //checkbox: selected - revision v4. default - v2
+                ->onValue(4)
+                ->offValue(2)
+            ->onChangeMethod('setRevisionToCreateNetpingLinks'), //put revision to session
+            Text::make('Название', 'name'), //name of the netping
+            Text::make('IP', 'ip') //ip address: reactive field
+                ->reactive(function(Fields $fields, ?string $value): Fields {
+                    $revision = session('revision') ? session('revision') : 2; //get revision number from session
+                /*
+                 * Helper array: formatting netping links using revision
+                 */
+                    $params = [
+                      'power_state' => env('NETPING_LOGIN') . $value . ($revision == 2 ? env('POWER_STATE') : env('POWER_STATE_v4')),
+                      'door_state' =>  env('NETPING_LOGIN') . $value . ($revision == 2 ? env('DOOR_STATE') : env('DOOR_STATE_v4')),
+                      'alarm_state' => env('NETPING_LOGIN') . $value . ($revision == 2 ? env('ALARM_STATE') : env('ALARM_STATE_v4')),
+                      'netping_state' => env('NETPING_LOGIN') . $value . ($revision == 2 ? env('NETPING_STATE') : env('NETPING_STATE_v4')),
+                      'alarm_control' => env('NETPING_LOGIN') . $value . ($revision == 2 ? env('ALARM_CONTROL') : env('ALARM_CONTROL_v4')),
+                      'alarm_switch_v4' => $revision == 4 ? env('NETPING_LOGIN') . $value .  env('ALARM_SWITCH_V4') : '',
+                    ];
+                    foreach ($params as $key => $value) //fill fields using helper array and foreach loop
+                    {
+                        $reactive_fields = tap($fields, static fn ($fields) => $fields
+                        ->findByColumn($key)
+                        ?->setValue($value)->value());
+                    }
+                    return $reactive_fields;
+                }
+            ),
+            Text::make('Камера', 'camera_ip'), //camera ip. not required
+            Divider::make('Ссылки на управление точкой (заполняются автоматически)'), //divider for section with netping links
+            Text::make('Статус питания', 'power_state')->reactive(), //power state link
+            Text::make('Статус двери', 'door_state')->reactive(), //door state link
+            Text::make('Статус сирены', 'alarm_state')->reactive(), //alarm state link
+            Text::make('Состояние охраны', 'netping_state')->reactive(), //secure state link
+            Text::make('Управление охраной', 'alarm_control')->reactive(), //secure on/off link
+            Text::make('Отключение сирены (только v4)', 'alarm_switch_v4')->reactive(), //alarm on/off link (for revision v4)
+        ];
+    }
+
+    /*
+     * Put Checkbox value to the session.
+     * This method using on onChangeMethod property.
+     * onChangeMethod requires ONLY public methods!
+     */
+    public function setRevisionToCreateNetpingLinks(MoonShineRequest $request)
+    {
+        session()->put('revision', $request->get('value'));
+    }
 
     public function rules(Model $item): array
     {
