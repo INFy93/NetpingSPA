c2d1074e705bd86a5296d650d9073830cb81656b
INFy93 <infy93@gmail.com>
2024-04-17 15:54:24 +0300

    belongsTo

--- app/Models/Temperature.php
+++ app/Models/Temperature.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Temperature extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'bdcom_id',
+        'temperature'
+    ];
+
+    public function bdcom(): BelongsTo
+    {
+        return $this->belongsTo(Bdcom::class);
+    }
 }
