d698dd7a5f2e1053121d40447fb4708e5bd15728
INFy93 <infy93@gmail.com>
2024-04-18 14:30:07 +0300

    fix array for graphs

--- app/MoonShine/Pages/Graphs/TemperatureGraphs.php
+++ app/MoonShine/Pages/Graphs/TemperatureGraphs.php
@@ -6,11 +6,14 @@ namespace App\MoonShine\Pages\Graphs;
 
 use App\Models\Bdcom;
 use App\Models\Temperature;
+use MoonShine\Decorations\Flex;
 use MoonShine\Decorations\Heading;
 use MoonShine\Decorations\Tab;
 use MoonShine\Decorations\Tabs;
+use MoonShine\Fields\Select;
 use MoonShine\Fields\Text;
 use MoonShine\Metrics\LineChartMetric;
+use MoonShine\MoonShineRequest;
 use MoonShine\Pages\Page;
 use phpDocumentor\Reflection\Types\Object_;
 
@@ -23,7 +26,6 @@ class TemperatureGraphs extends Page
         ];
     }
 
-    private $bdcoms;
     private $charts;
 
     private function getBdcomNames()
@@ -36,40 +38,69 @@ class TemperatureGraphs extends Page
         return $this->title ?: 'BDCOM - графики';
     }
 
-    public function beforeRender(): void
+    public function generateCharts()
     {
-        $this->bdcoms = $this->getBdcomNames();
+        $bdcoms = $this->getBdcomNames();
         $this->charts = new \stdClass();
         $chart = [];
-
-        foreach ($this->bdcoms as $b)
-        {
+        $bdcom_list = [];
+        foreach ($bdcoms as $b) {
+            $bdcom_list[] = [
+                $b->id => $b->bdcom_name,
+            ];
             $chart[] = [
                 Heading::make($b->bdcom_name)->customAttributes(['class' => 'mt-3']),
                 LineChartMetric::make($b->bdcom_name)
-                ->line([
-                    $b->bdcom_name => Temperature::query()
-                        ->where('bdcom_id', $b->id)
-                        ->selectRaw('temperature as temp, DATE_FORMAT(created_at, "%d.%m.%Y") as date')
-                        ->groupBy('date')
-                        ->pluck('temp', 'date')
-                        ->toArray()
-                ], '#ec4176')
+                    ->line([
+                        $b->bdcom_name => Temperature::query()
+                            ->where('bdcom_id', $b->id)
+                            ->where('temperature', '!=', 0)
+                            ->selectRaw('temperature as temp, DATE_FORMAT(created_at, "'.session('sort_date').'") as date')
+                            ->groupBy('date')
+                            ->pluck('temp', 'date')
+                            ->toArray()
+                    ], '#ec4176')->when(session('sort_date'))
 
             ];
 
         }
+        //dd($bdcom_list);
+        $select = Flex::make([
+            Select::make('Диапазон выборки')->options([
+                '%d.%m.%Y %H:%i' => 'По часам',
+                '%d.%m.%Y' => 'По дням',
+                '%d.%m' => 'По неделям'
+            ])
+                ->onChangeMethod('sortByPeriod')
+                ->setValue(session('sort_date') ?: '%d.%m.%Y %H:%i'),
+            Select::make('Выбрать BDCOM')->options(
+                $bdcom_list
+            ),
+        ]);
 
+        array_unshift($chart, $select);
         $chart = collect($chart);
 
         $flattened = $chart->flatten();
 
         $this->charts = $flattened->all();
 
+        return $this->charts;
+    }
+
+    public function beforeRender(): void
+    {
+        $this->generateCharts();
     }
 
     public function components(): array
-	{
-		return (array)$this->charts;
-	}
+    {
+        return (array)$this->generateCharts();
+    }
+
+    public function sortByPeriod(MoonShineRequest $request)
+    {
+        session()->put('sort_date', $request->get('value'));
+        header("Refresh:0");
+    }
 }
