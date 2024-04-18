ce4f55f985a4d123a5d2f6aba2612d482c37a432
INFy93 <infy93@gmail.com>
2024-04-18 14:10:13 +0300

    add vue view for graphs

---
+++ resources/js/Pages/Graphs/TemperatureGraphs.vue
@@ -0,0 +1,18 @@
+<template>
+    <Head title="Графики"/>
+
+    <AuthenticatedLayout>
+        Тут будут графики
+    </AuthenticatedLayout>
+</template>
+
+<script>
+import {Head} from "@inertiajs/vue3";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+
+export default {
+    name: "TemperatureGraphs",
+    components: {AuthenticatedLayout, Head}
+}
+</script>
+
