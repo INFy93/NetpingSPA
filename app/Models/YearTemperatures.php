2ec93f472f55384c99bbc6eb636f033c3222651f
INFy93 <infy93@gmail.com>
2024-04-24 11:14:43 +0300

    models and migrations for helper tables

---
+++ app/Models/YearTemperatures.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class YearTemperatures extends Model
+{
+    use HasFactory;
+}
