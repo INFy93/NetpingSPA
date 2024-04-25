b97d7ec69513815b2441c4bcc20f1385773aae63
INFy93 <infy93@gmail.com>
2024-04-25 11:53:09 +0300

    change relationship -> hasOne

--- app/Models/Temperature.php
+++ app/Models/Temperature.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 
 class Temperature extends Model
@@ -16,8 +18,13 @@ class Temperature extends Model
         'temperature'
     ];
 
-    public function bdcom(): BelongsTo
+    public function bdcom(): HasOne
     {
-        return $this->belongsTo(Bdcom::class);
+        return $this->hasOne(PivotNetpingBdcom::class, 'bdcom_id', 'bdcom_id');
+    }
+
+    public function scopeWhereDateBetween($query,$fieldName,$fromDate,$todate)
+    {
+        return $query->whereDate($fieldName,'>=',$fromDate)->whereDate($fieldName,'<=',$todate);
     }
 }
