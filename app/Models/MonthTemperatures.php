95701e18fc1a20eba91d3453f436791410d666d4
INFy93 <infy93@gmail.com>
2024-04-25 11:51:46 +0300

    add models for avg temps tables

--- app/Models/MonthTemperatures.php
+++ app/Models/MonthTemperatures.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class MonthTemperatures extends Model
 {
     use HasFactory;
+
+    public function scopeWhereDateBetween($query,$fieldName,$fromDate,$todate)
+    {
+        return $query->whereDate($fieldName,'>=',$fromDate)->whereDate($fieldName,'<=',$todate);
+    }
 }
