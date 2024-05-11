f7ffb0b4307ec778a011093d813912ce1331b861
INFy93 <infy93@gmail.com>
2024-05-11 14:05:57 +0300

    add 'paginate' to collections

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\ExtendCollectionProvider::class,
     App\Providers\MoonShineServiceProvider::class,
+    App\Providers\ExtendCollectionProvider::class,
 ];
